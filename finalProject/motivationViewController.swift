e60b3e2c6a53aa16cec25d26f7877e181c04fef5
Mylene <marananmylene2@gmail.com>
2021-07-22 14:11:19 -0700

    serotonin + motivation draft

---
+++ finalProject/motivationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  motivationViewController.swift
+//  finalProject
+//
+//  Created by Mylene Maranan on 7/22/21.
+//
+
+import UIKit
+
+class motivationViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func randomQuote(_ sender: Any) {
+    }
+    
+}
