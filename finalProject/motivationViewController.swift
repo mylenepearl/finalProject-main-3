84a0a147515c18dd76e231b4b65165c9361c684d
Mylene <marananmylene2@gmail.com>
2021-07-22 16:41:33 -0700

    motivation + self care [complete]

--- finalProject/motivationViewController.swift
+++ finalProject/motivationViewController.swift
@@ -28,7 +28,7 @@ class motivationViewController: UIViewController {
     */
     @IBAction func randomQuote(_ sender: Any) {
         
-        let array = ["Fall seven times, get up eight", "Growth is growth, no matter how small", "I am excited to learn something new today", "I am where I need to be right now","With each step you take, you will grow stronger","Keep going, you may be almost there without even knowing"]
+        let array = ["Fall seven times, get up eight", "Growth is growth, no matter how small", "I am excited to learn something new today", "I am where I need to be right now","With each step you take, you grow stronger","I never lose. Either I win or I learn", "Put good out into the world", "Mental health is a journey, not a destination","Be the best version of you", "You grow beautifully in your own way", "Seek what sets your soul on fire", "Give yourself credit for how far you've come", "One day at a time", "Go for it", "Bloom with grace", "Relax, refresh, reconnect", "Celebrate every tiny victory", "The world needs what you got"]
         label.text = array.randomElement()
                      
 
